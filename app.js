a5136a9ba24642927220496f4dfc111f652d43b3
Kenneth Carroll <kenneth.james.carroll@gmail.com>
2020-09-17 17:55:27 -0400

    included router into app

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-// module imports
+// node_module imports
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+
+// user modules import
+const userRouter = require('./routes/user-routes');
 
 //env config
 dotenv.config();
@@ -22,6 +25,8 @@ mongoose.connect(`mongodb://${process.env.DB_ADDR}:${process.env.DB_PORT}/brew`)
         console.log(err);
     })
 
+// add router middleware
+app.use('/', userRouter);
 
 // app execution
 app.listen(process.env.PORT);
