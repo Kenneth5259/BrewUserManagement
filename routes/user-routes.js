13ca9fecd2d6e94990361bc8e5f5ef3ed8c02b87
Kenneth Carroll <kenneth.james.carroll@gmail.com>
2020-09-17 19:13:52 -0400

    Updated some comments in the router

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -4,5 +4,9 @@ const express = require('express');
 // define a router
 const router = express.Router();
 
+// router get methods
+
+// router post methods
+
 // export the router
 module.exports = router;
