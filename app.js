fc2c0e2ab4f596cedb60ad25be09a6fda6809366
Kenneth Carroll <kenneth.james.carroll@gmail.com>
2020-09-17 17:49:53 -0400

    added database connection

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 // module imports
 const express = require('express');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 //env config
 dotenv.config();
 
 // app intiialization
 const app = express();
+
+// database connection
+mongoose.connect(`mongodb://${process.env.DB_ADDR}:${process.env.DB_PORT}/brew`)
+    .then(() => {
+        
+        // handle successful connection
+        console.log("Connection Successful");
+    })
+    .catch((err) => {
+
+        // catch unsuccessful connection
+        console.log(err);
+    })
+
 
 // app execution
 app.listen(process.env.PORT);
